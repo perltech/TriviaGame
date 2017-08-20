9b164cff7019aa81689036a20033c26b2b66fa3d
perltech <noah.perlmutter@gmail.com>
2017-08-20 17:35:10 -0600

    add timer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 
 var questions = [
-
+  $("#question1"), $("#question2")
 ];
 
 var rightAnswers = 0;
@@ -28,41 +28,91 @@ function notAnswered() {
 }
 
 
+var count = 120;
+
+var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+
+function timer()
+{
+  count -= 1;
+  if (count <= 0) {
+     clearInterval(counter);
+     //counter ended, do something here
+    for(i = 0; i < questions.length; i++) {
+    getScore(questions[i]);
+    return;
+    };
+    $("#timer").html(count + ": secounds left");
+     
+  }
+}
+
 
 // I think this needs to change to a switch switch($("input[type=radio]:checked", "#myForm").val())
 // case: "rightAnswer" {
 //      correct();
 //      break;}
-function getScore() {
-  for(i = 0; i < 2; i++) {
+function getScore(questions) {
+  
+
+
+     switch($("input[type=radio]:checked", "#myForm").val()) {
+      case "rightAnswer":
+        correct();
+        console.log(rightAnswers + " right");
+        break;
+
+      case "wrongAnswer":
+        wrong();
+        console.log(wrongAnswers + "wrong");
+        break;
+
+      default:
+        notAnswered();
+        console.log(skipped + "no answer");
+        break;
+    }
+
+
+
+
+
     
     // if ($("[input[type=radio]").checked === "rightAnswer")
     // if ($("label[input[type=radio]]").val("rightAnswer")
-    if ($("input[type=radio]:checked", "#myForm").val() === "rightAnswer") { //this makes sense, I think
-      correct();
-      // localStorage.setItem("answer-right", rightAnswers);
-      console.log(rightAnswers + "right");
-      break;
-    }
-    else if ($("input[type=radio]:checked", "#myForm").val() === "wrongAnswer") {
-      wrong();
-      // Wrong answers need to be divided by number of extra wrong answers per loop, probably 2 or 3
-      // Can this be accomplished as an algorithm using %?
-      console.log(wrongAnswers + "wrong");
-      break;
-    }
-    else {
-      notAnswered();
-      console.log(skipped + "no answer");
-      break;
-    }
-  }
-};
+    // if ($("input[type=radio]:checked", "#myForm").val() === "rightAnswer") { //this makes sense, I think
+    //   correct();
+    //   // localStorage.setItem("answer-right", rightAnswers);
+    //   console.log(rightAnswers + "right");
+    //   break;
+    // }
+    // else if ($("input[type=radio]:checked", "#myForm").val() === "wrongAnswer") {
+    //   wrong();
+    //   // Wrong answers need to be divided by number of extra wrong answers per loop, probably 2 or 3
+    //   // Can this be accomplished as an algorithm using %?
+    //   console.log(wrongAnswers + "wrong");
+    //   break;
+    // }
+    // else {
+    //   notAnswered();
+    //   console.log(skipped + "no answer");
+    //   break;
+    // }
+  };
 
+$("#quiz-page").load('quiz-page.html', function() {
+  timer();
+})
+
+//timer();
 
 
 $("#done").click(function() {
-  getScore();  
+
+  for(i = 0; i < questions.length; i++) {
+    getScore(questions[i]);
+  };
+
   window.location.href='results-page.html';
   //Clear Interval
 });
@@ -79,20 +129,6 @@ $("#results-page").load('results-page.html', function() {
 
 
 // Try again button needs to blow away all the session storage 
-
-
-
-
-
-
-
-// $('#myForm input').on('change', function() {
-//    alert($('input[name=radioName]:checked', '#myForm').val()); 
-// });
-
-
-
-
 
 
 }); // document ready
